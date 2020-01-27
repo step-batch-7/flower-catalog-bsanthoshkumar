17d10f6d19374e4a9163073a60ce74bf9dfe4906
santhosh <santhosh.kumar@thoughtworks.com>
2020-01-27 15:26:25 +0530

    serving guestBook.html from server.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
+const { readFileSync, existsSync, statSync } = require('fs');
 const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
 const STATIC_FOLDER = `${__dirname}/public`;
+const { loadTemplate } = require('./lib/viewTemplate');
 
 const isFileExists = path => {
-  return fs.existsSync(path) && fs.statSync(path);
+  return existsSync(path) && statSync(path);
 };
 
 const serveStaticFile = req => {
@@ -13,7 +14,7 @@ const serveStaticFile = req => {
   const extension = path.split('.').pop();
   const response = new Response();
   response.statusCode = 200;
-  response.body = fs.readFileSync(path);
+  response.body = readFileSync(path);
   response.setHeader('Content-Type', CONTENT_TYPES[extension]);
   response.setContentLength();
   return response;
@@ -24,8 +25,34 @@ const serveHomePage = req => {
   return serveStaticFile(req);
 };
 
+const createRow = line => {
+  return `
+  <tr>
+  <td>${line.date}</td>
+  <td>${line.name}</td>
+  <td>${line.comment}</td>
+</tr>`;
+};
+
+const getComments = () => {
+  const commentsList = JSON.parse(readFileSync('./comments.json'));
+  return commentsList.reduce(createRow, '');
+};
+
+const serveGuestPage = req => {
+  const comments = getComments();
+  const response = new Response();
+  response.statusCode = 200;
+  response.body = loadTemplate('guestBook.html', { COMMENTS: comments });
+  response.setHeader('Content-Type', CONTENT_TYPES.html);
+  response.setContentLength();
+  return response;
+};
+
 const findHandler = req => {
   if (req.method === 'GET' && req.url === '/') return serveHomePage;
+  if (req.method === 'GET' && req.url === '/guestBook.html') return serveGuestPage;
+  if (req.method === 'POST' && req.url === 'saveComment') return saveComment;
   if (req.method === 'GET') return serveStaticFile;
   return () => new Response();
 };
