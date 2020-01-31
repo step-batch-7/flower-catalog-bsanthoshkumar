b3698ead640d3bbff4ff0c1935a825ace352101e
santhosh <santhosh.kumar@thoughtworks.com>
2020-01-31 10:12:27 +0530

    added  test cases for all html files

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -11,3 +11,36 @@ describe('GET home page', function() {
       .expect('Content-Length', '792', done);
   });
 });
+
+describe('GET index page', function() {
+  it('should get ./public/index.html path', function(done) {
+    request(app.serve.bind(app))
+      .get('/index.html')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect('Content-Length', '792', done);
+  });
+});
+
+describe('GET abeliophyllum page', function() {
+  it('should get ./public/abeliophyllum.html path', function(done) {
+    request(app.serve.bind(app))
+      .get('/abeliophyllum.html')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect('Content-Length', '1534', done);
+  });
+});
+
+describe('GET ageratum page', function() {
+  it('should get ./public/ageratum.html path', function(done) {
+    request(app.serve.bind(app))
+      .get('/ageratum.html')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect('Content-Length', '1217', done);
+  });
+});
